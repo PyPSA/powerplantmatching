a0eb176d06d23e4804d37dbf49ab02368dacb49b
Jonas Hoersch <jonas@chaoflow.net>
2016-10-26 19:10:09 +0200

    Hofmann instead of Hoffmann :)

--- powerplant_collection.py
+++ powerplant_collection.py
@@ -1,4 +1,4 @@
-## Copyright 2015-2016 Fabian Hoffmann (FIAS), Jonas Hoersch (FIAS)
+## Copyright 2015-2016 Fabian Hofmann (FIAS), Jonas Hoersch (FIAS)
 
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
