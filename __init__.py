1922c5068854b06962dd5179dfce0d3d4415148f
Jonas Hoersch <jonas@chaoflow.net>
2016-10-26 18:03:48 +0200

    Add gpl3 license header and improve README

--- __init__.py
+++ __init__.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 18 15:25:17 2016
+## Copyright 2015-2016 Fabian Hoffmann (FIAS), Jonas Hoersch (FIAS)
 
-@author: godula
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+powerplantmatching
+
+A set of tools for cleaning, standardising and combining multiple
+power plant databases.
 """
 
