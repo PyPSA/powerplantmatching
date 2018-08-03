085835c7d6950f84637cc1e9f901180e57abb3d9
Fabian Gotzens <f.gotzens@fz-juelich.de>
2018-08-03 11:40:58 +0200

    __init__.py: PEP8 code style

--- powerplantmatching/__init__.py
+++ powerplantmatching/__init__.py
@@ -1,17 +1,17 @@
-## Copyright 2015-2016 Fabian Hofmann (FIAS), Jonas Hoersch (FIAS)
+# Copyright 2015-2016 Fabian Hofmann (FIAS), Jonas Hoersch (FIAS)
 
-## This program is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License as
-## published by the Free Software Foundation; either version 3 of the
-## License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 powerplantmatching
@@ -23,6 +23,7 @@ power plant databases.
 
 from __future__ import absolute_import
 
-from . import config, cleaning, data, heuristics, export, matching, utils, collection, plot
+from . import (config, cleaning, data, heuristics, export, matching, utils,
+               collection, plot)
 
 import logging
