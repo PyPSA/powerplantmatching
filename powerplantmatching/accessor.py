dda23bc0a3828c5a4d777995a931a9435859fab5
Fabian Gotzens <f.gotzens@fz-juelich.de>
2020-03-27 10:54:08 +0100

    accessor: adaptation to new pycodestyle style rules: no logical changes, only editorial.

--- powerplantmatching/accessor.py
+++ powerplantmatching/accessor.py
@@ -8,6 +8,7 @@ Created on Tue Jul 16 18:09:56 2019
 
 import pandas
 
+
 @pandas.api.extensions.register_dataframe_accessor("powerplant")
 class PowerPlantAccessor():
     """
@@ -26,6 +27,7 @@ class PowerPlantAccessor():
     entsoe.powerplant.plot_aggregated()
 
     """
+
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
@@ -36,13 +38,13 @@ class PowerPlantAccessor():
                         convert_alpha2_to_country)
     from .export import to_pypsa_names
     from .heuristics import (
-            extend_by_non_matched, scale_to_net_capacities,
-            fill_missing_commyears, extend_by_VRE, fill_missing_duration,
-            rescale_capacities_to_country_totals, fill_missing_decommyears)
+        extend_by_non_matched, scale_to_net_capacities,
+        fill_missing_commyears, extend_by_VRE, fill_missing_duration,
+        rescale_capacities_to_country_totals, fill_missing_decommyears)
     from .cleaning import clean_powerplantname, aggregate_units
     from .matching import reduce_matched_dataframe
 
-    def plot_aggregated(self, by=['Country', 'Fueltype'], figsize=(12,20),
+    def plot_aggregated(self, by=['Country', 'Fueltype'], figsize=(12, 20),
                         **kwargs):
         """
         Plotting function for fast inspection of the capacity distribution.
@@ -61,7 +63,7 @@ class PowerPlantAccessor():
         """
         import matplotlib.pyplot as plt
         from .utils import lookup, convert_country_to_alpha2
-        subplots = True if len(by)>1 else False
+        subplots = True if len(by) > 1 else False
         fig, ax = plt.subplots(figsize=figsize, **kwargs)
         df = lookup(convert_country_to_alpha2(self._obj), by=by)
         df = df.unstack().rename_axis(None) if subplots else df
@@ -77,17 +79,15 @@ class PowerPlantAccessor():
 
     def match_with(self, df, labels=None, use_saved_matches=False,
                    config=None, reduced=True, **dukeargs):
-        from .matching import combine_multiple_datasets, \
-                              reduce_matched_dataframe
+        from .matching import (combine_multiple_datasets,
+                               reduce_matched_dataframe)
         from .utils import to_list_if_other
 
         dfs = [self._obj] + to_list_if_other(df)
         res = combine_multiple_datasets(
-                dfs, labels, use_saved_matches=use_saved_matches,
-                config=config, **dukeargs)
+            dfs, labels, use_saved_matches=use_saved_matches, config=config,
+            **dukeargs)
         if reduced:
             return res.pipe(reduce_matched_dataframe, config=config)
         return res
     pass
-
-
