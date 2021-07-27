34d024dc152817d27f2e81fe982544e6266fc76c
Fabian <fab.hof@gmx.de>
2021-07-27 10:02:02 +0200

    setup.py add openpyxl

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=['numpy','scipy','pandas>=0.24.0','networkx>=1.10',
                       'pycountry', 'xlrd', 'seaborn', 'pyyaml >=5.1.0',
                       'requests', 'matplotlib', 'geopy', 'xlrd', 'entsoe-py',
-                      'deprecation'],
+                      'deprecation', 'openpyxl'],
     extras_require={
         "docs": ["numpydoc", "sphinx", "sphinx_rtd_theme", "nbsphinx", "sphinx-automodapi"],
         "dev": ["pre-commit", "pytest", "pytest-cov"],
