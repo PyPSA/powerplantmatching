f345028f02b9512335565b58e7e22c819a8cc994
Fabian <fab.hof@gmx.de>
2022-04-07 14:51:07 +0200

    improve config for better matching results

--- matching_analysis/compare-with-entsoe-stats.py
+++ matching_analysis/compare-with-entsoe-stats.py
@@ -12,8 +12,9 @@ from powerplantmatching.cleaning import gather_fueltype_info
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
 config = pm.get_config()
-powerplants = pm.powerplants()
+powerplants = pm.powerplants(update=True)
 powerplants = powerplants.powerplant.convert_country_to_alpha2()
+powerplants = powerplants[powerplants.lat.notnull()]
 
 client = EntsoePandasClient(api_key=config["entsoe_token"])
 
@@ -35,9 +36,18 @@ stats = pd.DataFrame({c: parse(c) for c in powerplants.Country.unique()})
 fueltypes = gather_fueltype_info(pd.DataFrame({"Fueltype": stats.index}), ["Fueltype"])
 stats = stats.groupby(fueltypes.Fueltype.values).sum().unstack()
 
+# Manual correction on the statistics
+
+# https://de.wikipedia.org/wiki/Liste_von_Wasserkraftwerken_in_der_Schweiz?oldformat=true
+stats.loc["CH", "Hydro"] = 18000
+# https://www.bmk.gv.at/dam/jcr:f0bdbaa4-59f2-4bde-9af9-e139f9568769/Energie_in_OE_2020_ua.pdf
+stats.loc["AT", "Hydro"] = 14600
+
 totals = powerplants.powerplant.lookup().fillna(0)
 
-sources = config["matching_sources"]
+sources = [
+    s if isinstance(s, str) else list(s).pop() for s in config["matching_sources"]
+]
 dbs = {
     s.title(): getattr(pm.data, s)()
     .powerplant.convert_country_to_alpha2()
@@ -46,13 +56,13 @@ dbs = {
     for s in sources
 }
 
+empty = pd.Series(0, index=totals.index)  # only for cosmetics
 compare = (
-    pd.concat({"Statistics": stats, "Totals": totals, "NONE": 0, **dbs}, axis=1).fillna(
+    pd.concat({"Statistics": stats, "Totals": totals, "": empty, **dbs}, axis=1).fillna(
         0
     )
     / 1000
 )
-
 
 for c in compare.index.unique(0):
     df = compare.loc[c]
@@ -61,4 +71,20 @@ for c in compare.index.unique(0):
     ax.set_ylabel("Capacity [GW]")
     ax.set_title(cc.convert(c, to="name"))
     fig.tight_layout()
-    fig.savefig("figures/country-comparison/" + c + ".png")
+    fig.savefig("figures/country-comparison/" + c + ".png", dpi=150)
+    plt.close()
+
+# biggest differences
+
+diff = compare.Statistics - compare.Totals
+diff = diff.loc[:, list(set(compare.index.unique(1)) - {"Wind", "Solar"})]
+diff = diff.sort_values(ascending=False)
+diff.index = diff.index.get_level_values(0) + " " + diff.index.get_level_values(1)
+
+fig, ax = plt.subplots(figsize=(5, 20))
+diff[diff.abs() > 2].plot.barh(ax=ax, zorder=3)
+ax.set_xlabel("Capacity difference (stats - ppm) [GW]")
+ax.set_title(cc.convert(c, to="name"))
+ax.grid(True, zorder=2)
+fig.tight_layout()
+fig.savefig("figures/capacity-diff-per-country-and-fueltype.png", dpi=150)
