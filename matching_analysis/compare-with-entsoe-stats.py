fb9a912d8ab7560cf91e3f330c8b7f6ac3b4ba10
Fabian <fab.hof@gmx.de>
2022-04-27 13:53:36 +0200

    improve matching analysis

--- matching_analysis/compare-with-entsoe-stats.py
+++ matching_analysis/compare-with-entsoe-stats.py
@@ -11,10 +11,20 @@ from powerplantmatching.cleaning import gather_fueltype_info
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
+UPDATE = True
+
+
 config = pm.get_config()
-powerplants = pm.powerplants(update=False)
-powerplants = powerplants.powerplant.convert_country_to_alpha2()
-powerplants = powerplants[powerplants.lat.notnull()]
+
+if UPDATE:
+    powerplants = pm.powerplants(update=True)
+    powerplants = powerplants.powerplant.convert_country_to_alpha2()
+    powerplants = powerplants.powerplant.fill_geoposition()
+else:
+    powerplants = pm.powerplants()
+    powerplants = powerplants.powerplant.convert_country_to_alpha2()
+
+# powerplants = powerplants[powerplants.lat.notnull()]
 
 client = EntsoePandasClient(api_key=config["entsoe_token"])
 
@@ -42,28 +52,81 @@ stats = stats.groupby(fueltypes.Fueltype.values).sum().unstack()
 stats.loc["CH", "Hydro"] = 18000
 # https://www.bmk.gv.at/dam/jcr:f0bdbaa4-59f2-4bde-9af9-e139f9568769/Energie_in_OE_2020_ua.pdf
 stats.loc["AT", "Hydro"] = 14600
+# https://www.statista.com/statistics/496283/total-electricity-generation-capacity-uk/
+stats.loc["GB", "Natural Gas"] = 37000  # (adding ~10GW)
+stats.loc["GB", "Other"] = 1400  # (subtracting ~10GW)
+
 
 totals = powerplants.powerplant.lookup().fillna(0)
 
 sources = [s if isinstance(s, str) else list(s)[0] for s in config["matching_sources"]]
-dbs = {
-    s.title(): getattr(pm.data, s)()[lambda df: df.lat.notnull()]
+
+input_dbs = {
+    s.title(): getattr(pm.data, s)()
     .powerplant.convert_country_to_alpha2()
     .powerplant.lookup()
     .fillna(0)
     for s in sources
 }
+output_dbs = {
+    s.title(): powerplants[
+        powerplants.projectID.apply(lambda ds: s in ds)
+    ].powerplant.lookup()
+    for s in sources
+}
 
+# These are the capacities which come out of the data files
 empty = pd.Series(0, index=totals.index)  # only for cosmetics
-compare = (
-    pd.concat({"Statistics": stats, "Totals": totals, "": empty, **dbs}, axis=1).fillna(
-        0
-    )
-    / 1000
-)
+d = {"Statistics": stats, "Totals": totals, "": empty, **input_dbs}
+in_compare = pd.concat(d, axis=1).fillna(0) / 1000
 
-for c in compare.index.unique(0):
-    df = compare.loc[c]
+# These are the capacities which went into the resulting data
+d = {"Statistics": stats, "Totals": totals, "": empty, **output_dbs}
+out_compare = pd.concat(d, axis=1).fillna(0) / 1000
+
+
+# ---------------------------------------------------------------------------- #
+#                                  Differences                                 #
+# ---------------------------------------------------------------------------- #
+
+diff = (out_compare.Totals - out_compare.Statistics).to_frame("Difference")
+for s in sources:
+    ds = out_compare[s.title()] / out_compare.Totals * 100
+    diff[s.title() + " (%)"] = ds.fillna(0)
+
+diff = diff[out_compare.Statistics != 0]
+diff = diff.loc[:, list(set(out_compare.index.unique(1)) - {"Wind", "Solar"}), :]
+diff.index = diff.index.get_level_values(0) + " " + diff.index.get_level_values(1)
+
+df = (diff[diff.Difference > 1]).sort_values("Difference", ascending=False)
+print(f"\nOverestimated Capacities (clip at 1 GW): \n\n{df.round(2)}")
+
+df = (diff[diff.Difference < -1]).sort_values("Difference", ascending=True)
+print(f"\nMissing Capacities (clip at 1 GW): \n\n{df.round(2)}")
+
+
+country_diff = diff.Difference.groupby(diff.index.str[:2]).sum()
+
+df = (country_diff[country_diff > 0]).sort_values(ascending=False)
+print(f"\nOverestimated Capacities per Country: \n\n{df.round(2)}")
+
+df = (country_diff[country_diff < 0]).sort_values(ascending=True)
+print(f"\nMissing Capacities per Country: \n\n{df.round(2)}")
+
+# ---------------------------------------------------------------------------- #
+#                                country figures                               #
+# ---------------------------------------------------------------------------- #
+
+fig, ax = plt.subplots(figsize=(5, 20))
+diff[diff.abs() > 2].plot.barh(ax=ax, zorder=3)
+ax.set_xlabel("Capacity difference (stats - ppm) [GW]")
+ax.grid(True, zorder=2)
+fig.tight_layout()
+fig.savefig("figures/capacity-diff-per-country-and-fueltype.png", dpi=150)
+
+
+for c in in_compare.index.unique(0):
+    df = in_compare.loc[c]
     fig, ax = plt.subplots(figsize=(15, 5))
     df.plot.bar(ax=ax)
     ax.set_ylabel("Capacity [GW]")
@@ -71,18 +134,3 @@ for c in compare.index.unique(0):
     fig.tight_layout()
     fig.savefig("figures/country-comparison/" + c + ".png", dpi=150)
     plt.close()
-
-# biggest differences
-
-diff = compare.Statistics - compare.Totals
-diff = diff.loc[:, list(set(compare.index.unique(1)) - {"Wind", "Solar"})]
-diff = diff.sort_values(ascending=False)
-diff.index = diff.index.get_level_values(0) + " " + diff.index.get_level_values(1)
-
-fig, ax = plt.subplots(figsize=(5, 20))
-diff[diff.abs() > 2].plot.barh(ax=ax, zorder=3)
-ax.set_xlabel("Capacity difference (stats - ppm) [GW]")
-ax.set_title(cc.convert(c, to="name"))
-ax.grid(True, zorder=2)
-fig.tight_layout()
-fig.savefig("figures/capacity-diff-per-country-and-fueltype.png", dpi=150)
--- matching_analysis/update-matches.py
+++ matching_analysis/update-matches.py
@@ -1,7 +1,9 @@
-# Run this from this directory:
+# Run this from the root directory
 
 import powerplantmatching as pm
 
 df = pm.powerplants(update=False)
 df = df[df.lat.notnull()].reset_index(drop=True)
-df.to_csv("../powerplants.csv", index_label="id")
+df = df.powerplant.fill_geopositions()
+df = df.drop_duplicates(["Name", "Fueltype", "Country"])
+df.to_csv("powerplants.csv", index_label="id")
