47f8271382460e5bcc6d61dc2ce5fcd9da5130e8
davide-f <fioritidavidesubs@gmail.com>
2022-11-17 18:01:46 +0100

    Add standard raw and update options for compatibility

--- powerplantmatching/data.py
+++ powerplantmatching/data.py
@@ -1649,11 +1649,15 @@ def GEM_GGPT(raw=False, update=False, config=None):
     return df
 
 
-def EXTERNAL_DATABASE(config=None):
+def EXTERNAL_DATABASE(raw=False, update=True, config=None):
     """
     Importer for external custom databases.
     Parameters
     ----------
+    raw : boolean, default False
+        Whether to return the original dataset
+    update: bool, default True (unused)
+        Whether to update the data from the url.
     config : dict, default None
         Add custom specific configuration,
         e.g. powerplantmatching.config.get_config(target_countries='Italy'),
@@ -1663,6 +1667,10 @@ def EXTERNAL_DATABASE(config=None):
         return pd.DataFrame()
 
     df = pd.read_csv(config["EXTERNAL_DATABASE"]["fn"], low_memory=False)
+
+    if raw:
+        return df
+
     df = df.pipe(set_column_name, "EXTERNAL_DATABASE").pipe(config_filter, config)
 
     return df
